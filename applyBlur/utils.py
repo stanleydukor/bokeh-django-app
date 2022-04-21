16bea03a48613559307b278c29c5b5bb47746af5
Stanley Dukor <stanleydukor@gmail.com>
2022-04-21 03:55:02 -0700

    remove bloom function

--- applyBlur/utils.py
+++ applyBlur/utils.py
@@ -15,12 +15,6 @@ def applyBlur(img, size=(256, 256), radius=50):
   blurred = cv2.filter2D(img, -1, disk)
   return blurred
 
-def applyBloom(img, threshold, gain):
-  gray = cv2.cvtColor(img.astype('float32'), cv2.COLOR_BGR2GRAY)
-  mask = gray >= threshold
-  img[mask] *= gain
-  return img
-
 def lensBlur(img, radius=50, gamma=3.0):
   size = radius * 4
   gammaCorrectedImg = np.power(img, gamma)
